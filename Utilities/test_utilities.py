3157609e62afeebfdf6de5ccba78a04343ba47bd
Matt Sargent <sagshome@gmail.com>
2022-10-17 20:50:54 -0400

    Down to 98%  found a bug,  but going on vacation so checking in.

---
+++ Utilities/test_utilities.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+from os import makedirs, walk
+from pathlib import Path
+from piexif import ImageIFD, ExifIFD, dump, insert, load
+from shutil import copyfile
+from typing import Union
+from PIL import Image, ImageDraw
+
+DATE_SPEC = datetime(1961, 9, 27)
+DIR_SPEC = Path(str(DATE_SPEC.year)).joinpath(str(DATE_SPEC.month)).joinpath(str(DATE_SPEC.day))
+
+
+def copy_file(in_file: Path, out_path: Path, cleanup: bool = False, new_name: str = None) -> Path:
+
+    if not out_path.exists():
+        makedirs(out_path)
+
+    new_name = in_file.name if not new_name else new_name
+    new_path = out_path.joinpath(new_name)
+    copyfile(str(in_file), new_path)
+
+    if cleanup:
+        in_file.unlink()
+    return new_path
+
+
+def count_files(path: Path):
+    count = 0
+    for _, _, files_list in walk(path):
+        for file in files_list:
+            count += 1
+    return count
+
+
+def create_file(name: Path, data: str = None, empty: bool = False) -> Path:
+
+    if not name.parent.exists():
+        makedirs(name.parent)
+
+    f = open(name, 'w+')
+    if not empty:
+        if not data:
+            f.write(str(name))
+        else:
+            f.write(data)
+    f.close()
+    return name
+
+
+def create_image_file(path: Path, date: Union[datetime, None], text: str = None, small: bool = False):
+    """
+    Use this to create an Image File  (It will respect the type..., use text to make unique)
+    :param path:  -> what / where to save the image to
+    :param date: -> the datetime to set,  None has not date date
+    :param text: -> Data to write into the image,  default is path.name
+    :param small:  Set the image size to 360x360
+    :return:
+    """
+
+    if not path.parent.exists():
+        makedirs(path.parent)
+
+    size = (360, 360) if small else (400, 400)
+    text = text if text else path.stem
+    exif_dict = {}
+
+    canvas = Image.new('RGB', size, 'white')
+    img_draw = ImageDraw.Draw(canvas)
+    img_draw.text((70, 250), text, fill='green')
+    if date:
+        exif_dict['0th'] = {}
+        exif_dict['0th'][ImageIFD.DateTime] = date.strftime("%Y:%m:%d %H:%M:%S")
+    canvas.save(path, exif=dump(exif_dict))
+
+
+def set_date(original_file: Path, new_date: Union[datetime, None]):
+    """
+    Given a physical file,  move the file to the input directory
+    original_file:  The file to process
+    new_date: date_string to put into file
+    move_to_input: If set,  copy the file to this location.
+    :return: None
+    """
+    exif_dict = load(str(original_file))
+    if not new_date:
+        if ImageIFD.DateTime in exif_dict['0th']:
+            del (exif_dict['0th'][ImageIFD.DateTime])
+        if ExifIFD.DateTimeOriginal in exif_dict['Exif']:
+            del (exif_dict['Exif'][ExifIFD.DateTimeOriginal])
+        if ExifIFD.DateTimeDigitized in exif_dict['Exif']:
+            del (exif_dict['Exif'][ExifIFD.DateTimeDigitized])
+    else:
+        new_date = new_date.strftime("%Y:%m:%d %H:%M:%S")
+        exif_dict['0th'][ImageIFD.DateTime] = new_date
+        exif_dict['Exif'][ExifIFD.DateTimeOriginal] = new_date
+        exif_dict['Exif'][ExifIFD.DateTimeDigitized] = new_date
+
+    # Save changes
+    exif_bytes = dump(exif_dict)
+    insert(exif_bytes, str(original_file))
+
+
